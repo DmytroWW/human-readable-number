607c65ad11986e2ff101b101358e6c2f50848c4e
DmytroWW <veksbrightside@gmail.com>
2024-09-04 13:11:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const singleDigits = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teensDigits = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tensDigits = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 10) return singleDigits[number];
+    if (number < 20) return teensDigits[number - 10];
+    if (number < 100) {
+        return (
+            tensDigits[Math.floor(number / 10)] +
+            (number % 10 !== 0 ? " " + singleDigits[number % 10] : "")
+        );
+    }
+    if (number < 1000) {
+        return (
+            singleDigits[Math.floor(number / 100)] +
+            " hundred" +
+            (number % 100 !== 0 ? " " + toReadable(number % 100) : "")
+        );
+    }
+};
